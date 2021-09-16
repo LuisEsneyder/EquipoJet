41d96c064a8ff733bba43e4a225f3ffa8a55e1b6
Luis Fernández <luisexneider1999@gmail.com>
2021-09-16 14:56:17 -0500

    Agrego las clases de usuario y gastos

---
+++ FinanzasMiHogar.App.Dominio/Entidades/Usuarios.cs
@@ -0,0 +1,14 @@
+using system;
+namespace FinananzasMiHogar
+{
+    /// <summary> Class <c>Usuarios</c>
+    /// Modela a los usuarios en el sistema
+    /// </summary>
+    public class Usuarios
+    {
+        //Atributos
+        public string nombreUsuario {get; set;}
+        public string correoElectronico {get; set;}
+        public int topeGastos {get; set;} 
+    }
+}
