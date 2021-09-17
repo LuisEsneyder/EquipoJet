14f43432015ffd079295eff75b9e68b5ef73d104
Luis Fernández <luisexneider1999@gmail.com>
2021-09-17 17:16:52 -0500

    Agrego la clase de Gastos, Periodos de los gastos y Tipos de gastoss

---
+++ FinanzasMiHogar.App.Dominio/Entidades/PeriodoGastos.cs
@@ -0,0 +1,13 @@
+using system;
+namespace FinananzasMiHogar.App.Dominio
+{
+    /// <summary>
+    /// Lista de Periodos para los gastos
+    /// </summary>
+    public enum PeriodoGastos
+    {
+        mensual,
+        quincenal,
+        semanal
+    }
+}
---
+++ FinanzasMiHogar.App.Dominio/Entidades/TipoGasto.cs
@@ -0,0 +1,12 @@
+using system;
+namespace FinananzasMiHogar.App.Dominio
+{
+    /// <summary>
+    /// Lista de los tipos de gastos
+    /// </summary>
+    public enum Name
+    {
+        suscripcion,
+        serviciosPublicos
+    }
+}
